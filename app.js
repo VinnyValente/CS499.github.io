8f660df9e71202a753c73e97d672e7b68f518d32
VinnyValente <valentevinny356@yahoo.com>
2024-01-17 15:18:25 -0500

    required hbs

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const hbs = require('hbs');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
